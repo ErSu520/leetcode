fc99e1cfaeaf0ca29c4d5f2f53abc271590106bd
ersuer <727534356@qq.com>
2022-09-05 17:08:30 +0800

    约瑟夫环还没有做出来

---
+++ src/com/flash/easy/LastNumberOfCircle.java
@@ -0,0 +1,81 @@
+package com.flash.easy;
+
+import com.flash.common.ListNode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 约瑟夫环
+ * 0,1,···,n-1这n个数字排成一个圆圈，从数字0开始，每次从这个圆圈里删除第m个数字（删除后从下一个数字开始计数）。
+ * 求出这个圆圈里剩下的最后一个数字。
+ *
+ * 例如，0、1、2、3、4这5个数字组成一个圆圈，从数字0开始每次删除第3个数字，则删除的前4个数字依次是2、0、4、1，因此最后剩下的数字是3。
+ *
+ */
+public class LastNumberOfCircle {
+
+    /**
+     * 使用链表来存储所有的数字，自然模拟
+     * 超时
+     * @param n
+     * @param m
+     * @return
+     */
+    static int solution1(int n, int m){
+        ListNode head = new ListNode(0);
+        ListNode rear = head;
+        for(int i=1;i<n;i++){
+            rear.next = new ListNode(i);
+            rear = rear.next;
+        }
+        rear.next = head;
+        head = rear;
+        m -= 1;
+        while (head != head.next){
+            for(int i=0;i<m;i++){
+                head = head.next;
+            }
+            head.next = head.next.next;
+        }
+        return head.value;
+    }
+
+    /**
+     * 用ArrayList来存放所有的数字，ArrayList会自动去除被删除的位置
+     * 耗时太长
+     * @param n
+     * @param m
+     * @return
+     */
+    static int solution2(int n, int m){
+        List<Integer> nums = new ArrayList<>(n);
+        for(int i=0;i<n;i++){
+            nums.add(i);
+        }
+        int index = n-1;
+        while (nums.size() > 1){
+            index = (index + m) % nums.size();
+            nums.remove(index);
+            index -= 1;
+        }
+        return nums.get(0);
+    }
+
+    /**
+     *
+     * @param n
+     * @param m
+     * @return
+     */
+    static int solution(int n, int m){
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(5, 1));
+    }
+
+}
--- src/com/flash/easy/ReverseWord.java
+++ src/com/flash/easy/ReverseWord.java
@@ -2,14 +2,46 @@ package com.flash.easy;
 
 public class ReverseWord {
 
-    static String solution(String s){
+    static String solution1(String s){
         StringBuilder builder = new StringBuilder(s.length());
+        char pre = '~';
         for(int i=0;i<s.length();i++){
             char c = s.charAt(i);
-//            if()
+            if(c == ' ' && pre == ' '){
+                continue;
+            }
             builder.append(c);
+            pre = c;
         }
-        return "";
+        return builder.toString();
+    }
+
+    static String solution(String s){
+        StringBuilder builder = new StringBuilder(s.length());
+        boolean flag = false;
+        String[] strs = s.split(" ");
+        for(int i=strs.length-1;i>-1;i--){
+            if(strs[i].length() != 0){
+                if(flag) builder.append(" ");
+                builder.append(strs[i]);
+                flag = true;
+            }
+        }
+        return builder.toString();
+    }
+
+    static String solution2(String s){
+        char[] string = new char[s.length()];
+        int index = -1;
+        char cur;
+        for(int i=0;i<s.length();i++){
+            cur = s.charAt(i);
+            if(cur == ' ' && (index == -1 || string[index] == ' ')){
+                continue;
+            }
+            string[++index] = cur;
+        }
+        return new String(string);
     }
 
     public static void main(String[] args) {
