b2edccaa93322c01a1133a6d67ee3d5b83b38e3b
ersuer <727534356@qq.com>
2022-09-07 10:36:52 +0800

    树的共同祖先节点

---
+++ src/com/flash/common/TreeNode.java
@@ -0,0 +1,14 @@
+package com.flash.common;
+
+import java.util.Stack;
+
+public class TreeNode {
+     public int val;
+     public TreeNode left;
+     public TreeNode right;
+     public TreeNode(int x) { val = x; }
+
+     public static TreeNode createTree(Integer[] treeNumber){
+          return null;
+     }
+}
