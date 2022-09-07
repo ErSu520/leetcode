5c1c8a8d025e59858f385d025badf26196862ab3
ersuer <727534356@qq.com>
2022-09-07 14:47:57 +0800

    完成斐波那契数列算法

--- src/com/flash/easy/Fibonacci.java
+++ src/com/flash/easy/Fibonacci.java
@@ -2,12 +2,22 @@ package com.flash.easy;
 
 import java.util.Arrays;
 
+/**
+ * 写一个函数，输入 n ，求斐波那契（Fibonacci）数列的第 n 项（即 F(N)）。斐波那契数列的定义如下：
+ *
+ * F(0) = 0, F(1)= 1
+ * F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
+ *
+ * 斐波那契数列由 0 和 1 开始，之后的斐波那契数就是由之前的两数相加而得出。
+ * 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+ */
 public class Fibonacci {
 
     public static int solution(int n) {
         if(n == 0) return 0;
         if(n == 1) return 1;
 
+        n++;
         int[] array = new int[n];
         for(int i=0;i<n;i++){
             if(i == 0) {
@@ -18,10 +28,9 @@ public class Fibonacci {
                 array[i] = 1;
                 continue;
             }
-            array[i] = (array[n-1] + array[n-2]) % 1000000007;
+            array[i] = (array[i-1] + array[i-2]) % 1000000007;
         }
-        System.out.println(Arrays.toString(array));
-        return (array[n-1] + array[n-2]) % 1000000007;
+        return array[n-1];
     }
 
     public static void main(String[] args) {
