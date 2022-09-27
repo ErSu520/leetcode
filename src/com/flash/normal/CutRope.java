1ce4310c3bd9284bb0fe25048b900eab7c7bcbca
ersuer <727534356@qq.com>
2022-09-27 11:33:20 +0800

    剪绳子

--- src/com/flash/normal/CutRope.java
+++ src/com/flash/normal/CutRope.java
@@ -1,5 +1,8 @@
 package com.flash.normal;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），每段绳子的长度记为 k[0],k[1]...k[m-1] 。
  * 请问 k[0]*k[1]*...*k[m-1] 可能的最大乘积是多少？
@@ -9,19 +12,23 @@ public class CutRope {
 
     static int max = 1;
 
-    static int next(int result, int number){
-        for(int i =1;i<number;i++){
-            int maybe = i * next(result * i, number - i);
-            System.out.println(maybe);
-            if(max < maybe){
-                max = maybe;
-                System.out.println(max);
-            }
+    static int solution(int n) {
+        if(n < 1){
+            return 0;
         }
-        return 1;
+        return rope(n, new ArrayList<>(0), 1);
+    }
+
+    static int rope(int n, List<Integer> list, int multi){
+        for(int i=0;i<=n;i++){
+            List<Integer> array = new ArrayList<>(list);
+            array.add(i);
+
+        }
+        return -1;
     }
 
     public static void main(String[] args){
-        System.out.println(next(1, 10));
+        System.out.println(solution(10));
     }
 }
